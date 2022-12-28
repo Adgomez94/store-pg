a4c9c5406e2186b7ead1cf5afc4060e270e57cad
Adrian Duran <duranadria77@gmail.com>
2022-12-28 15:43:37 +0000

    Added connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './module/products/products.module';
 
+const typeOrmConfig = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5433,
+  username: 'root',
+  password: 'root',
+  database: 'store-postgress',
+  entities: [`${__dirname}/**/entities/**/*{.js,.ts}`],
+  synchronize: true,
+  subscribers: [],
+  logging: true,
+} as TypeOrmModuleOptions;
+
 @Module({
-  imports: [ProductsModule],
+  imports: [TypeOrmModule.forRoot({ ...typeOrmConfig }), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
